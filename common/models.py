fe54e15f8c757e95d1bc9877ca92ab2d8eb2443b
chmin4 <164356097+chmin4@users.noreply.github.com>
2025-05-26 09:09:34 +0900

    Update models.py

--- common/models.py
+++ common/models.py
@@ -1,12 +1,14 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
+
 class UploadedFile(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     file = models.FileField(upload_to='uploads/')
-    assigned_name = 
-models.CharField(max_length=255, blank=false)
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    assigned_name = models.CharField(max_length=255)
+
+    def save(self, *args, **kwargs):
+        if not self.assigned_name:
+            raise ValueError("이름을 지정해주세요")
+        super().save(*args, **kwargs)
     

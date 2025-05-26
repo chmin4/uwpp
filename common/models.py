0d8b828c070130c335530ea2a78fd0347ff52bdb
chmin4 <164356097+chmin4@users.noreply.github.com>
2025-05-26 09:05:08 +0900

    upload_func_01

--- common/models.py
+++ common/models.py
@@ -6,5 +6,7 @@ from django.contrib.auth.models import User
 class UploadedFile(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     file = models.FileField(upload_to='uploads/')
+    assigned_name = 
+models.CharField(max_length=255, blank=false)
     uploaded_at = models.DateTimeField(auto_now_add=True)
     

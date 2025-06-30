25393440104899b50d6bcb718e6e2662a99e2dfe
chmin4 <164356097+chmin4@users.noreply.github.com>
2025-07-01 01:30:16 +0900

    Update urls.py

--- common/urls.py
+++ common/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('upload/',views.upload_file, name='upload'),
     path('download/<int:file_id>',views.download_file, name='download_file'),
     path('delete/<int:file_id>/', views.delete_file, name='delete_file'),
-    path('access-ide/',views.code_ide_redirect,name='secure-ide-view'),
+
 ]
